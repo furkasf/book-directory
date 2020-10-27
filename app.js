7987046553f840e37eb537971310353eccc6a58d
unknown <furkantural3328@hotmail.com>
2020-10-27 22:15:38 +0300

    partial seted

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const mongoose = require('mongoose');
 const Book = require('./model/book.js')
 //express server
 const app = express();
-const key = '';//maybe ı collect all data in json server
-mongoose.connect()
+const url = 'mongodb+srv://furkan:JKts5yPNhjvYSki9@cluster0.l9fic.mongodb.net/Cluster0<?retryWrites=true&w=majority';//maybe ı collect all data in json server
+mongoose.connect(key, { useNewUrlParser: true })
     .then(() => app.listen(3000))
     .catch(err => console.log(err))
 
 //middleware
 app.use(express.urlencoded({extended : true}))
-app.use(express.static())
+app.use(express.static('public'))
 
 //view engine
-app.set('view wngine', 'ejs')
+app.set('view engine', 'ejs')
 
 //routers
-
+app.get('/', (req, res) => res.render('home'))
